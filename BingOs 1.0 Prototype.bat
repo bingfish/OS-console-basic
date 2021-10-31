d97661940db5838ec8faddc0057d04a4243c399b
bingfish <82204478+bingfish@users.noreply.github.com>
2021-10-31 12:54:14 +0000

    Add files via upload
    First BingOS Release, type help in console for all commands.
    Please report bugs  to help the future of BingOS
    WINDOWS ONLY, as it runs in the Windows Command Line

---
+++ BingOs 1.0 Prototype.bat
@@ -0,0 +1,36 @@
+@echo off
+echo Welcome to the BingOS configuration menu
+pause
+:start
+echo Enter command
+set /p car=
+if %car% == ip goto ipc
+if %car% == help goto help
+if %car% == dir goto dir
+if %car% == configservices goto cfs
+:unknown
+echo Bad Command or File name, ignoring given command.
+timeout 1
+goto :start
+:ipc
+ipconfig
+goto :start
+:help
+echo HELP INTERFACE. Commands:
+echo ip = view IP Address configuration information
+echo help = view help information
+echo dir= view directory information
+echo configservices shows the currently installed version of BingOS
+goto :start
+:dir
+echo Opening DIR interface...
+timeout 1
+dir
+goto :start
+:cfs
+echo Loading...
+timeout 2
+echo System installed: BingOS Prototype 1.0 Stable Git release, Bingfish 2021 Open source
+echo Thank you for using BingOS.
+echo Closing System Info...
+goto :start
